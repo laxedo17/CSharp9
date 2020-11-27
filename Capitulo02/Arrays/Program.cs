815debd637f90fd5ea2ab6c680282b3e693827d2
laxedo17 <saracyan78@hotmail.com>
2020-11-27 14:26:43 +0100

    Fin Capitulo02

--- Capitulo02/Arrays/Program.cs
+++ Capitulo02/Arrays/Program.cs
@@ -6,7 +6,23 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] nomes;//pode referencia un array calquera de strings (cadenas de texto)
+
+            //asignando memoria para catro strings nun array
+            nomes = new string[4];
+
+            //almacenando obxetos nas posicions do indice (index)
+            nomes[0] = "Carol";
+            nomes[1] = "Mercedes Ben";
+            nomes[2] = "Dolores Fortes de Barriga";
+            nomes[3] = "Pierre";
+
+            //facendo loop para recorrer os nomes
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                //saca por pantalla o obxeto na posicion do indice
+                Console.WriteLine(nomes[i]);
+            }
         }
     }
 }
